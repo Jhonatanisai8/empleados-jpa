0071c7367669d9e6525281bb85697bd258400311
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-01 17:50:35 -0500

    insertando algunos datos desde la clase main

--- src/sistema/empleados/jpa/SistemaEmpleadosJpa.java
+++ src/sistema/empleados/jpa/SistemaEmpleadosJpa.java
@@ -1,9 +1,15 @@
 package sistema.empleados.jpa;
 
+import sistema.empleados.jpa.dao.PersonaDao;
+
 public class SistemaEmpleadosJpa {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        PersonaDao persona1 = new PersonaDao();
+        System.out.println(persona1.insertarPersona("Samanta Yamilet", "Juares Alarcon", 12, "999888231"));
+        System.out.println(persona1.insertarPersona("Kiara Pamela", "Alarcon Sosa", 17, "922888231"));
+        System.out.println(persona1.insertarPersona("Erika Rosa", "Salvador", 56, "999123231"));
+        
     }
 
 }
